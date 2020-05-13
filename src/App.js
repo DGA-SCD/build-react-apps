77c027ab3dacd0713d067ed5237c7fde1577d4c4
zkan <zkan.cs@gmail.com>
2020-05-14 00:22:16 +0700

    :recycle: Separate components into their own files :bear:

--- src/App.js
+++ src/App.js
@@ -1,104 +1,11 @@
 import React from 'react'
-import { StopWatch } from './StopWatch'
-import { nowUTC, convertUTCtoLocal } from './utils'
 import axios from 'axios'
+import { nowUTC } from './utils'
+import { NavBar } from './NavBar'
+import { NoTask } from './NoTask'
+import { TaskCreator } from './TaskCreator'
+import { TaskList } from './TaskList'
 import './App.css'
-
-const NavBar = (props) => {
-  const username = window.sessionStorage.getItem('username')
-  return (
-    <nav role='navigation' className='navbar fixed-top navbar-dark bg-dark'>
-      <a className='navbar-brand' href='.'>
-        Timely
-      </a>
-      <div style={{ color: '#fff' }}>{username}</div>
-    </nav>
-  )
-}
-
-const TaskCreator = (props) => {
-  return (
-    <div className='row'>
-      <div className='col-7'>
-        <input
-          type='text'
-          placeholder='What are you working on?'
-          className='form-control form-control-lg'
-          name='task'
-          value={props.taskName}
-          onChange={props.onTaskChange}
-        />
-      </div>
-      <div className='form-group'>
-        <select
-          className='form-control form-control-lg custom-select'
-          id='category'
-          style={{ height: '45px' }}
-          onChange={props.onCategoryChange}
-        >
-          <option disabled='' selected=''>
-            Category
-          </option>
-          <option selected={props.category === 'study'}>study</option>
-          <option selected={props.category === 'workout'}>workout</option>
-          <option selected={props.category === 'housekeeping'}>housekeeping</option>
-        </select>
-      </div>
-      <div className='col'>
-        {props.isStarted === true ? (
-          <button type='button' className='btn btn-danger btn-lg' onClick={props.onAddClicked}>
-            Stop
-          </button>
-        ) : (
-          <button type='button' className='btn btn-success btn-lg' onClick={props.onAddClicked}>
-            Start
-          </button>
-        )}
-      </div>
-      <StopWatch isWatchStarted={props.isStarted} />
-    </div>
-  )
-}
-
-const NoTask = () => {
-  return (
-    <div className='text-center p-5 my-3 rounded shadow-sm text-dark'>
-      <span>No task yet</span>
-    </div>
-  )
-}
-
-const TaskList = (props) => {
-  return (
-    <div className='my-3 bg-white rounded shadow-sm'>
-      <div>
-        {props.allTasks.map((task) => {
-          const startedLocalDateTime = convertUTCtoLocal(task.startedAt)
-          const endedLocalDateTime = convertUTCtoLocal(task.endedAt)
-          return (
-            <div
-              className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'
-              key={task.id}
-            >
-              <div className='col'>
-                <span>{task.name}</span>
-                <span className='ml-2 badge badge-info'>{task.category}</span>
-              </div>
-              <div className='col'>{task.username}</div>
-              <div className='col'>{startedLocalDateTime.date}</div>
-              <div className='col'>{`${startedLocalDateTime.time} - ${endedLocalDateTime.time}`}</div>
-              <div className=''>
-                <button className='btn btn-danger' onClick={() => props.onRemoveClicked(task.id)}>
-                  Remove
-                </button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
 
 class App extends React.Component {
   state = {
---
+++ src/NoTask.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NoTask = () => {
+  return (
+    <div className='text-center p-5 my-3 rounded shadow-sm text-dark'>
+      <span>No task yet</span>
+    </div>
+  )
+}
