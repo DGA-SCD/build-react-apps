feac81fa678fcc68468da60411f64bc286434bde
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-16 16:09:59 +0700

    Separate style to file :baby_chick:

---
+++ src/components/atoms/CategoryItem/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledListItem = styled.li`
+  display: inline-block;
+  margin: 0.25em;
+  padding: 0.5em 1em;
+  border: 1px solid #343a40;
+  border-radius: 0.25em;
+`
