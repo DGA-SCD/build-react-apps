c0340216d6e28a06803d1e522a62efde2e47b5e9
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-16 23:58:50 +0700

    :sparkles: Show categories from context :baby_chick:

--- src/components/atoms/CategorySelectBox/index.js
+++ src/components/atoms/CategorySelectBox/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { connect } from 'react-redux'
 
 import { storeCategoryId } from '../../../store/actions'
+import { CategoriesContext } from '../../../contexts'
 
-class CategorySelectBox extends React.Component {
-  render() {
-    return (
-      <select
-        className='form-control form-control-lg custom-select'
-        id='category'
-        style={{ height: '45px' }}
-        onChange={(e) => this.props.storeCategoryId(e.target.value)}
-      >
-        <option value={1}>Study</option>
-        <option value={2}>Workout</option>
-        <option value={3}>Housekeeping</option>
-      </select>
-    )
-  }
+const CategorySelectBox = () => {
+  const { categories } = useContext(CategoriesContext)
+
+  return (
+    <select
+      className='form-control form-control-lg custom-select'
+      id='category'
+      style={{ height: '45px' }}
+      onChange={(e) => this.props.storeCategoryId(e.target.value)}
+    >
+      {categories.map((category) => (
+        <option key={category.id} value={category.name}>
+          {category.name}
+        </option>
+      ))}
+    </select>
+  )
 }
 
 const mapStateToProps = (state) => ({
