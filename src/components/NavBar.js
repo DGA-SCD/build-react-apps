790bfbb3b1fe2bfb49c04cb7af1aca7c267fd175
zkan <zkan.cs@gmail.com>
2020-05-16 13:57:47 +0700

    :lipstick: Update NavBar's JSX :bear:

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
 export const NavBar = () => {
-  // const username = window.sessionStorage.getItem('username')
   return (
-    <nav role='navigation' className='navbar fixed-top navbar-dark bg-dark'>
-      <a className='navbar-brand' href='.'>
-        Timely
+    <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+      <a class='navbar-brand' href='/'>
+        Keep It later!
       </a>
-      <div style={{ color: '#fff' }}>zkan</div>
+      <button
+        class='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarCollapse'
+        aria-controls='navbarCollapse'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span class='navbar-toggler-icon'></span>
+      </button>
+      <div class='collapse navbar-collapse' id='navbarCollapse'>
+        <ul class='navbar-nav mr-auto'>
+          <li class='nav-item active'>
+            <a class='nav-link' href='/'>
+              Home <span class='sr-only'>(current)</span>
+            </a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='/categories'>
+              Categories
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
