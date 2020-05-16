e025d0c030df9ee7bbfb50f83e25902fbe930a2c
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-16 21:01:59 +0700

    :recycle: Refactor :baby_chick:

--- src/components/atoms/CategoryAddButton/index.js
+++ src/components/atoms/CategoryAddButton/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const CategoryAddButton = () => (
-  <button type='button' class='btn btn-success btn-lg'>
-    Add
-  </button>
-)
+export const CategoryAddButton = () => {
+  return (
+    <button type='button' class='btn btn-success btn-lg'>
+      Add
+    </button>
+  )
+}
