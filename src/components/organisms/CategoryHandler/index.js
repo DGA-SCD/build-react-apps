47c7b58386c86e5a058e94b22ffe60aa5b36ab8f
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-16 15:49:35 +0700

    Adjust category list using styled component :baby_chick:

--- src/components/organisms/CategoryHandler/index.js
+++ src/components/organisms/CategoryHandler/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { CategoryInput } from '../../atoms/CategoryInput'
 import { CategoryAddButton } from '../../atoms/CategoryAddButton'
 import { CategoryList } from '../../molecules/CategoryList'
+
+const StyledCategoryList = styled(CategoryList)`
+  padding: 0;
+`
 
 export const CategoryHandler = () => {
   const categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
@@ -19,7 +24,7 @@ export const CategoryHandler = () => {
       </div>
       <div class='row' style={{ marginTop: '20px' }}>
         <div class='col'>
-          <CategoryList categories={categories} />
+          <StyledCategoryList categories={categories} />
         </div>
       </div>
     </div>
