fce241a7eb7ac68e82a0b39e34455deefb047c96
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-17 00:19:47 +0700

    :recycle: Move base url to default :baby_chick:

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import axios from 'axios'
 
 import { Main } from './components/pages/Main'
 import { Categories } from './components/pages/Categories'
 import * as serviceWorker from './serviceWorker'
 import { reducer } from './store/reducers'
 import { CategoriesContextProvider } from './contexts'
+
+axios.defaults.baseURL = 'http://localhost:8000'
 
 const store = configureStore({
   reducer,
