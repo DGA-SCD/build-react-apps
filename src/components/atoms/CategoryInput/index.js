9c4ed9029424a02922cd911a08169f61058b3c65
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-16 20:06:20 +0700

    :rotating_light: Change class to className :baby_chick:

--- src/components/atoms/CategoryInput/index.js
+++ src/components/atoms/CategoryInput/index.js
@@ -4,7 +4,7 @@ export const CategoryInput = ({ value, onChange }) => (
   <input
     type='text'
     placeholder='Plase enter a new category'
-    class='form-control form-control-lg'
+    className='form-control form-control-lg'
     name='task'
     value={value}
     onChange={onChange}
