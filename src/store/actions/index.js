5e255f842bcdfd3141b305dc13add4343212e2c2
zkan <zkan.cs@gmail.com>
2020-05-17 10:18:24 +0700

    :mute: Remove console.log :bear:

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -12,7 +12,6 @@ export const addNewItemAsync = (item) => {
       })
       .then((response) => {
         const newItem = response.data
-        console.log(newItem)
         dispatch(addNewItem({ textInput: newItem.url, categoryName: newItem.category_name.String }))
       })
   }
