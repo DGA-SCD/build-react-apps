5d309cc29c085996da3e2e11607e12177a51da3c
zkan <zkan.cs@gmail.com>
2020-05-17 22:26:52 +0700

    :white_check_mark: Write tests for category reducer :bear:

---
+++ src/store/reducers/categories.test.js
@@ -0,0 +1,11 @@
+import { reducer } from './categories'
+
+describe('category reducer', () => {
+  it('should return an initial state', () => {
+    expect(reducer(undefined, {})).toEqual({ categoryId: 0 })
+  })
+
+  it('should store category ID', () => {
+    expect(reducer(undefined, { type: 'STORE_CATEGORY_ID', payload: 1 })).toEqual({ categoryId: 1 })
+  })
+})
