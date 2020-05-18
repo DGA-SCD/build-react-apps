1a39e9c0381ce3a4270cd1bae4e6419ae7224760
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-18 22:02:58 +0700

    :sparkles: Add storybook to category filter box :baby_chick:

---
+++ keep-it-later/src/components/CategoryFilterBox/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CategoryFilterBox } from '.'
+
+export default {
+  component: CategoryFilterBox,
+  title: 'CategoryfilterBox'
+}
+
+export const categoryFilterBox = () => <CategoryFilterBox />
+export const noCategory = () => <CategoryFilterBox />
