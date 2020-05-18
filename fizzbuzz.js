453babe8260a01271c305ecf723321a1b958fbf7
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-18 11:20:21 +0700

    :sparkles: Convert to es6

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,28 @@
-function fizzBuzz(number) {
-  if (number % 3 === 0 ) {
-    var result = 'Fizz'
+// function fizzBuzz(number) {
+//   if (number % 3 === 0 ) {
+//     var result = 'Fizz'
+//   }
+//
+//   if (number % 5 === 0) {
+//     result = result + 'Buzz'
+//   }
+//
+//   return result
+// }
+//
+// number = 30
+// console.log('Number: ' + number + ' Result: ' + fizzBuzz(number))
+
+const fizzBuzz = (number) => {
+  let result
+  if (number % 3 === 0) {
+    result = 'fizz'
   }
 
   if (number % 5 === 0) {
-    result = result + 'Buzz'
+    result = `${result}buzz`
   }
-
   return result
 }
-
-number = 30
-console.log('Number: ' + number + ' Result: ' + fizzBuzz(number))
+const number = 30
+console.log(`Number: ${number} Result: ${fizzBuzz(number)}`)
