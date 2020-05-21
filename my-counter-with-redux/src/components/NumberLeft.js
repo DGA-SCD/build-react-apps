498f1a5f3993299d82ee4966245a26d2e11a09ed
zkan <zkan.cs@gmail.com>
2020-05-21 20:41:51 +0700

    :sparkles: Map state to props for NumberLeft :bear:

--- my-counter-with-redux/src/components/NumberLeft.js
+++ my-counter-with-redux/src/components/NumberLeft.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export const NumberLeft = () => {
-  return <h2>1</h2>
+const NumberLeft = (props) => {
+  return <h2>{props.counter}</h2>
 }
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  }
+}
+
+export const ConnectedNumberLeft = connect(mapStateToProps, null)(NumberLeft)
