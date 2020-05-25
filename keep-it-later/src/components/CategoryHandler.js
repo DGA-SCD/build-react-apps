a6866f58deb005867f1056494bbf5cddd0e07d66
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-25 14:09:24 +0700

    :recycle: Use effect instead of componentDidMount() :baby_chick:

--- keep-it-later/src/components/CategoryHandler.js
+++ keep-it-later/src/components/CategoryHandler.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import { CategoryCreator } from './CategoryCreator'
 import { CategoryList } from './CategoryList'
 
+// define custom hook
+const useCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await axios.get('http://localhost:8000/categories')
+      console.log(response)
+      setCategories(response.data)
+    }
+
+    fetchCategories()
+  }, [])
+
+  return [categories, setCategories]
+}
+
 export const CategoryHandler = () => {
   // state = {
   //   category: '',
-  //   categories: []
+  //   categories: [] // ['Personal', 'Lame stuff']
   // }
-  const [category, setCategory] = useState()
+  const [category, setCategory] = useState('')
+  const [categories, setCategories] = useCategories()
 
   const onCategoryTextChanged = (event) => {
     // this.setState({ category: event.target.value })
@@ -18,6 +37,8 @@ export const CategoryHandler = () => {
   const onAddClicked = () => {
     // const newCategories = [...this.state.categories, this.state.category]
     // this.setState({ categories: newCategories })
+    const newCategories = [...categories, category]
+    setCategories(newCategories)
   }
 
   return (
